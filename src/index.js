e7638efb808fdbe7eec6ac0d8e5e22dbc637467e
Luiz <luizsouza1945@gmail.com>
2020-11-16 07:18:14 -0400

    Correcao

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS,PUT,PATCH,DELETE");
     app.use(cors());
     next();
 });
